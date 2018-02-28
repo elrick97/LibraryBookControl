847ea53a388799ce78672cc61ad867e3973d27f7
Ricardo Ramierz <ricardo.rmz.97@hotmail.com>
2018-02-28 08:22:06 -0600

    Initial Commit

---
+++ Proyecto2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proyecto2
+//
+//  Created by Ricardo Ramirez on 2/28/18.
+//  Copyright © 2018 Ricardo Ramirez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
